fa982de1ed436c9d178dfcecb08ff350338c9c7a
zilongwang1985 <zilongwang33@gmail.com>
2023-10-05 17:05:35 +0900

    aa

---
+++ src/main/java/com/example/com/HelloCon.java
@@ -0,0 +1,15 @@
+package com.example.com;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloCon {
+    @GetMapping("/aa")
+    public String hello() {
+
+        return "okcc";
+    }
+}
